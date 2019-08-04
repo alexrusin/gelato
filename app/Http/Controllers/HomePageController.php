3790b48df0c7eb0a91a36ff844230474bbcba73f
Alex Rusin <alexrusin_2000@yahoo.com>
2019-08-04 09:39:20 -0700

    fixed error in the name of Homepage controller

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomepageController extends Controller
+class HomePageController extends Controller
 {
     public function show() {
     	return view('frontend.homepage');
